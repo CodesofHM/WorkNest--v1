8b767c233bc50511e5cfa6639a4ce3d1fa087c44
Harshit Majithia <harshitmajithiya7@gmail.com>
2025-08-08 01:03:37 +0530

    API KEYS OF CLOUDINERY

--- worknest/server/config/cloudinary.js
+++ worknest/server/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'your_cloud_name',
-  api_key: 'your_api_key',
-  api_secret: 'your_api_secret',
+  cloud_name: 'dpxhrx0xd',
+  api_key: '137296375897677',
+  api_secret: 'UKSqvYuxejug78uirJwUX3v3phk',
 });
 
 module.exports = cloudinary;
